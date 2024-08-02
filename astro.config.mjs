624a628ad3326b5bec10c799a391f32aa95b31e2
AlexxScript <96507406+AlexxScript@users.noreply.github.com>
2024-08-01 21:42:36 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'alexxscript.com'
+  site: 'https://alexxscript.com/'
 });
